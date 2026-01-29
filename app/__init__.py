b97f8a5fac784cd1356a1d235c9098c6cba25643
tfermoso <tomas.fermoso@gmail.com>
2026-01-29 19:33:05 +0100

    modificaciones para desplegar en pythonanywhere

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
-from flask import Flask, request
+from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
+from pathlib import Path
 
 from .config import Config
 from .extensions import db, migrate, jwt
 
 def create_app():
-    load_dotenv()
+    # raíz del proyecto: .../api-padel
+    project_root = Path(__file__).resolve().parent.parent
+    load_dotenv(project_root / ".env")  # <-- clave
+
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app)  # útil para React luego
+    CORS(app)
 
     db.init_app(app)
-    migrate.init_app(app, db)  # migraciones via Flask-Migrate :contentReference[oaicite:6]{index=6}
+    migrate.init_app(app, db)
     jwt.init_app(app)
-    
+
     from .auth import auth_bp
     from .api import api_bp
     from .admin import admin_bp
@@ -23,11 +27,8 @@ def create_app():
     app.register_blueprint(api_bp, url_prefix="/api")
     app.register_blueprint(admin_bp, url_prefix="/admin")
 
-    
     @app.route("/")
     def index():
         return {"message": "API Padel funcionando!"}
-    
-  
 
     return app
--- app/config.py
+++ app/config.py
@@ -1,14 +1,22 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # .../marketplace-api
+BASE_DIR = Path(__file__).resolve().parent.parent  # .../api-padel
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-jwt")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///padel.db")
+
+    db_url = os.getenv("DATABASE_URL", "sqlite:///padel.db")
+
+    # si es sqlite relativa: sqlite:///padel.db -> sqlite:////home/.../api-padel/padel.db
+    if db_url.startswith("sqlite:///") and not db_url.startswith("sqlite:////"):
+        filename = db_url.replace("sqlite:///", "", 1)
+        db_path = (BASE_DIR / filename).resolve()
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path.as_posix()}"
+    else:
+        SQLALCHEMY_DATABASE_URI = db_url
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = str(BASE_DIR / os.getenv("UPLOAD_FOLDER", "uploads"))
     MAX_CONTENT_LENGTH = int(os.getenv("MAX_CONTENT_LENGTH_MB", "10")) * 1024 * 1024
-
-
